34965bb72c47b054d6654f901625f972c1a3adbb
jing <1318873422@qq.com>
2025-12-31 10:39:56 +0800

    refactor(schedule): 重构任务调度相关类结构
    - 将JobTask类重构为使用@Data注解，简化代码结构
    - 移除JSONObject依赖，改为直接使用字段存储任务信息
    - 修改Quartz类以适配新的JobTask结构
    - 优化日志输出使用新的字段名称

--- src/main/java/com/jing/admin/core/schedule/job/JobTask.java
+++ src/main/java/com/jing/admin/core/schedule/job/JobTask.java
@@ -5,6 +5,7 @@ import com.alibaba.fastjson2.JSONObject;
 import com.jing.admin.core.constant.ConstantEnum;
 import com.jing.admin.core.schedule.AbstractJobTask;
 import com.jing.admin.core.schedule.engine.TaskEngine;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.List;
@@ -14,23 +15,20 @@ import java.util.List;
  * @date 2024/6/7
  **/
 @Slf4j
-public class JobTask {
+@Data
+public class JobTask  {
+    private String id;
+    private String name;
+    /**
+     * 定时时间
+     */
+    private String coron;
+
+    private AbstractJobTask task;
     /**
      * 执行超时时间
      */
     private long runTimeout = 15 * 60 * 1000;
-    /**
-     * 作业实体
-     */
-    private JSONObject job;
-    /**
-     * 节点实体
-     */
-    private JSONObject workflow;
-    /**
-     * 启动参数
-     */
-    private JSONObject startParams;
 
     /**
      * 设置运行超时时间
@@ -42,16 +40,15 @@ public class JobTask {
     }
 
     public void run() {
-        log.info("[调度任务] 任务名称: {}", job.getString("jobName"));
-        AbstractJobTask task = null;
+        log.info("[调度任务] 任务名称: {}", name);
         try {
             //创建任务
             //创建执行引擎
             new TaskEngine(task, runTimeout).run();
         } catch (Exception e) {
-            log.info("[调度任务] name: {} 异常: {}", job.getString("jobName"), e.getMessage());
+            log.info("[调度任务] name: {} 异常: {}", name, e.getMessage());
         } finally {
-            log.info("[调度任务] name: {} 运行结果: {} 结束", job.getString("jobName"), task.getStatus());
+            log.info("[调度任务] name: {} 运行结果: {} 结束", name, task.getStatus());
             if (!ConstantEnum.SUCCESS.equals(task.getStatus())) {
             }
         }
--- src/main/java/com/jing/admin/core/schedule/job/Quartz.java
+++ src/main/java/com/jing/admin/core/schedule/job/Quartz.java
@@ -1,6 +1,5 @@
 package com.jing.admin.core.schedule.job;
 
-import com.alibaba.fastjson2.JSONObject;
 import lombok.Getter;
 import org.quartz.*;
 
@@ -10,29 +9,18 @@ import org.quartz.*;
  **/
 @Getter
 public class Quartz {
-    private String jobId;
-    private String cron;
+    private JobTask jobTask;
     private JobDataMap jobData;
 
-    public Quartz(String jobId, String jobName, String cron, JSONObject job) {
-        this.jobId = jobId;
-        this.cron = cron;
-        this.jobData = new JobDataMap();
-        jobData.put("jobName", jobName);
-        jobData.put("job", job);
-        jobData.put("workflow", job.getJSONObject("workflow"));
+    public Quartz(JobTask jobTask) {
+        this.jobTask = jobTask;
     }
-
-    public Quartz(JSONObject job) {
-        this(job.getString("jobId"), job.getString("jobName"), job.getString("jobCron"), job);
-    }
-
     public JobDetail restartJob() throws ClassNotFoundException {
-        return JobBuilder.newJob(JobScheduler.class).withIdentity(this.jobId).setJobData(this.jobData).storeDurably().build();
+        return JobBuilder.newJob(JobScheduler.class).withIdentity(this.jobTask.getId()).setJobData(this.jobData).storeDurably().build();
     }
 
     public Trigger restartTrigger() throws ClassNotFoundException {
-        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule(this.cron);
-        return TriggerBuilder.newTrigger().forJob(restartJob()).withIdentity(this.jobId).withSchedule(scheduleBuilder).build();
+        CronScheduleBuilder scheduleBuilder = CronScheduleBuilder.cronSchedule((this.jobTask.getCoron()));
+        return TriggerBuilder.newTrigger().forJob(restartJob()).withIdentity(this.jobTask.getId()).withSchedule(scheduleBuilder).build();
     }
 }
